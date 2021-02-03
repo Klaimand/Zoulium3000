d3d18fbe0abc9053b2c8126bee954fa7befdbebc
Clément Pagnier <clempagnier@yahoo.fr>
2021-02-03 19:56:24 +0100

    Avancement event

---
+++ SpeldaLike/Assets/KLD/KLD_Scripts/GameEvents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents Instance = null;
+
+    public event Action onGravityDisable;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void DisableGravity()
+    {
+        onGravityDisable?.Invoke();
+
+    }
+
+}
