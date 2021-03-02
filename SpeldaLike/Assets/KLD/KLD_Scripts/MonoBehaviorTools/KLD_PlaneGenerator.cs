37df30465ae51695db1d6c069d18bd55b5c43ee8
Clément Pagnier <clempagnier@yahoo.fr>
2021-03-02 15:22:31 +0100

    avancement sur le flat shading des planes

--- SpeldaLike/Assets/KLD/KLD_Scripts/MonoBehaviorTools/KLD_PlaneGenerator.cs
+++ SpeldaLike/Assets/KLD/KLD_Scripts/MonoBehaviorTools/KLD_PlaneGenerator.cs
@@ -11,6 +11,16 @@ public class KLD_PlaneGenerator : SerializedMonoBehaviour
 
     [SerializeField] Material material;
 
+    [SerializeField] MeshFilter meshNormalsToDraw;
+
+    private void Update()
+    {
+        for (int i = 0; i < meshNormalsToDraw.mesh.normals.GetLength(0); i++)
+        {
+            Debug.DrawRay(meshNormalsToDraw.mesh.vertices[i], meshNormalsToDraw.mesh.normals[i]);
+        }
+    }
+
     [Button]
     public void CreatePlane()
     {
@@ -39,18 +49,47 @@ public class KLD_PlaneGenerator : SerializedMonoBehaviour
 
     Vector3[] GenerateVertices()
     {
-        Vector3[] verticesInst = new Vector3[planeSquares.x * planeSquares.y * 4];
+        Vector3[] verticesInst = new Vector3[planeSquares.x * planeSquares.y * 6];
 
         for (int y = 0; y < planeSquares.y; y++)
         {
             for (int x = 0; x < planeSquares.x; x++)
             {
-                int cornerIndex = ((y * planeSquares.x) + x) * 4;
+                int cornerIndex = ((y * planeSquares.x) + x) * 6;
                 Vector3 cornerPosition = new Vector3(x * squareSize.x, 0f, y * squareSize.y);
+                if (x == 0 && y == 0)
+                {
+                    //cornerPosition = new Vector3(cornerPosition.x, 1f, cornerPosition.z);
+                }
+
+                //verticesInst[cornerIndex] = cornerPosition;
+                //verticesInst[cornerIndex + 1] = cornerPosition + Vector3.forward * squareSize.y;
+                //verticesInst[cornerIndex + 2] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x;
+                //verticesInst[cornerIndex + 3] = cornerPosition + Vector3.right * squareSize.x;
+                //verticesInst[cornerIndex + 4] = cornerPosition;
+                //verticesInst[cornerIndex + 5] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x;
+
+                //verticesInst[cornerIndex] = cornerPosition + Vector3.up * Random.value;
+                //verticesInst[cornerIndex + 1] = cornerPosition + Vector3.forward * squareSize.y + Vector3.up * Random.value;
+                //verticesInst[cornerIndex + 2] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x + Vector3.up * Random.value;
+                //verticesInst[cornerIndex + 3] = cornerPosition + Vector3.right * squareSize.x + Vector3.up * Random.value;
+                //verticesInst[cornerIndex + 4] = cornerPosition + Vector3.up * Random.value;
+                //verticesInst[cornerIndex + 5] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x + Vector3.up * Random.value;
+
                 verticesInst[cornerIndex] = cornerPosition;
                 verticesInst[cornerIndex + 1] = cornerPosition + Vector3.forward * squareSize.y;
                 verticesInst[cornerIndex + 2] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x;
                 verticesInst[cornerIndex + 3] = cornerPosition + Vector3.right * squareSize.x;
+                verticesInst[cornerIndex + 4] = cornerPosition;
+                verticesInst[cornerIndex + 5] = cornerPosition + Vector3.forward * squareSize.y + Vector3.right * squareSize.x;
+
+                verticesInst[cornerIndex] = verticesInst[cornerIndex] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex].x / (float)planeSquares.x, verticesInst[cornerIndex].z / (float)planeSquares.y);
+                verticesInst[cornerIndex + 1] = verticesInst[cornerIndex + 1] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex + 1].x / (float)planeSquares.x, verticesInst[cornerIndex + 1].z / (float)planeSquares.y);
+                verticesInst[cornerIndex + 2] = verticesInst[cornerIndex + 2] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex + 2].x / (float)planeSquares.x, verticesInst[cornerIndex + 2].z / (float)planeSquares.y);
+                verticesInst[cornerIndex + 3] = verticesInst[cornerIndex + 3] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex + 3].x / (float)planeSquares.x, verticesInst[cornerIndex + 3].z / (float)planeSquares.y);
+                verticesInst[cornerIndex + 4] = verticesInst[cornerIndex + 4] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex + 4].x / (float)planeSquares.x, verticesInst[cornerIndex + 4].z / (float)planeSquares.y);
+                verticesInst[cornerIndex + 5] = verticesInst[cornerIndex + 5] + Vector3.up * Mathf.PerlinNoise(verticesInst[cornerIndex + 5].x / (float)planeSquares.x, verticesInst[cornerIndex + 5].z / (float)planeSquares.y);
+
             }
         }
         print("generated " + verticesInst.GetLength(0) + " vertices");
@@ -65,7 +104,7 @@ public class KLD_PlaneGenerator : SerializedMonoBehaviour
         {
             for (int x = 0; x < planeSquares.x; x++)
             {
-                int verticeCornerIndex = ((y * planeSquares.x) + x) * 4;
+                int verticeCornerIndex = ((y * planeSquares.x) + x) * 6;
 
                 int triangleIntIndex = ((y * planeSquares.x) + x) * 6;
 
@@ -73,8 +112,8 @@ public class KLD_PlaneGenerator : SerializedMonoBehaviour
                 trianglesInst[triangleIntIndex + 1] = verticeCornerIndex + 1;
                 trianglesInst[triangleIntIndex + 2] = verticeCornerIndex + 2;
 
-                trianglesInst[triangleIntIndex + 3] = verticeCornerIndex;
-                trianglesInst[triangleIntIndex + 4] = verticeCornerIndex + 2;
+                trianglesInst[triangleIntIndex + 3] = verticeCornerIndex + 4;
+                trianglesInst[triangleIntIndex + 4] = verticeCornerIndex + 5;
                 trianglesInst[triangleIntIndex + 5] = verticeCornerIndex + 3;
             }
 
