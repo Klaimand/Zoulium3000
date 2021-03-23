3be28837195773f2584067bed8e9cd9c71de971d
Clément Pagnier <clempagnier@yahoo.fr>
2021-03-23 15:13:07 +0100

    avancement sur le tool, bientot fini inchallah

--- SpeldaLike/Assets/KLD/KLD_Editor/PolyplaneEditorWindow.cs
+++ SpeldaLike/Assets/KLD/KLD_Editor/PolyplaneEditorWindow.cs
@@ -5,12 +5,24 @@ using UnityEngine.UIElements;
 
 public class PolyplaneEditorWindow : EditorWindow
 {
+    //FIELDS
+    Vector2Field planeSquares;
+    Vector2Field squareSize;
+    FloatField seaLevel;
+
+    Button flatPlaneButton;
+    Button noisePlaneButton;
+
+    FloatField noiseStrenght;
+    FloatField noiseScale;
+    ObjectField materialField;
+
     [MenuItem("Tools/Polyplane Creator")]
     public static void ShowWindow()
     {
         var window = GetWindow<PolyplaneEditorWindow>();
         window.titleContent = new GUIContent("Plane Creator");
-        window.minSize = new Vector2(800, 600);
+        window.minSize = new Vector2(280, 300);
     }
 
     private void OnEnable()
@@ -18,5 +30,44 @@ public class PolyplaneEditorWindow : EditorWindow
         VisualTreeAsset original = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/KLD/KLD_Editor/PolyplaneEditorWindow.uxml");
         TemplateContainer treeAsset = original.CloneTree();
         rootVisualElement.Add(treeAsset);
+
+        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/KLD/KLD_Editor/PolyplaneEditorStyles.uss");
+        rootVisualElement.styleSheets.Add(styleSheet);
+
+        InitializeFields();
     }
+
+    void InitializeFields()
+    {
+        //VECTOR2
+        planeSquares = rootVisualElement.Q<Vector2Field>("plane-squares");
+        planeSquares.RegisterValueChangedCallback<Vector2>(OnPlaneSquaresValueChanged);
+
+        //BUTTONS
+        flatPlaneButton = rootVisualElement.Q<Button>("flat-plane-button");
+        noisePlaneButton = rootVisualElement.Q<Button>("noise-plane-button");
+
+        flatPlaneButton.RegisterCallback<ClickEvent>(ev => OnFlatPlaneButtonClick());
+        noisePlaneButton.RegisterCallback<ClickEvent>(ev => OnNoisePlaneButtonClick());
+
+
+    }
+
+    //void OnSearchTextChanged(ChangeEvent<string> evt)
+    void OnPlaneSquaresValueChanged(ChangeEvent<Vector2> evt)
+    {
+        //change plane squares in base class
+    }
+
+    void OnFlatPlaneButtonClick()
+    {
+        Debug.Log("Flat plane click");
+    }
+
+    void OnNoisePlaneButtonClick()
+    {
+        Debug.Log("Noise plane click");
+    }
+
+
 }
