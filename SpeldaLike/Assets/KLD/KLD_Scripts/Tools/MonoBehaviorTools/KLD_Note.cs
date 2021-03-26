c2d4a8014f901dd80a10a618c5129f1a630d3f5f
Clément Pagnier <clempagnier@yahoo.fr>
2021-03-26 17:44:14 +0100

    [MERGEABLE] Prefabs de LD

---
+++ SpeldaLike/Assets/KLD/KLD_Scripts/Tools/MonoBehaviorTools/KLD_Note.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_Note : MonoBehaviour
+{
+    [SerializeField, TextArea(5, 10)] string Note = "This is a note";
+}
