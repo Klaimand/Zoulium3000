f56c8bb1cad7712b3a5778dbbffb3e0382540ac7
damotime <r.sutralanut@rubika-edu.com>
2021-05-24 16:09:13 +0200

    Ennemis en attente d'intégration finale mais mergeable

---
+++ SpeldaLike/Assets/RSL/RSL_Ennemis/Acrobate/Acrobate_Folder/RSL_HitAcrobate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RSL_HitAcrobate : MonoBehaviour
+{
+    public GameObject Vfx;
+
+    void SalutatouscestDigidixlalegendeonline()
+    {
+        Instantiate(Vfx, transform.position, transform.rotation);
+    }
+
+
+}
