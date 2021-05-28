7f58fda507396040b0cd2e7ff91129b92aac958c
Clément Pagnier <clempagnier@yahoo.fr>
2021-05-28 15:42:31 +0200

    Pour rémi | test nouveau commit events

--- SpeldaLike/Assets/KLD/KLD_Scripts/KLD_PP8.cs
+++ SpeldaLike/Assets/KLD/KLD_Scripts/KLD_PP8.cs
@@ -5,29 +5,50 @@ using UnityEngine;
 public class KLD_PP8 : MonoBehaviour
 {
 
-    Transform player;
+    //Transform player;
+    Transform target;
 
-    [SerializeField] float maxAngleSpeed = 10f;
+    [SerializeField] float maxAngularSpeed = 10f;
+    [SerializeField] float timeToReachTargetAngle = 0.2f;
+
+    //[SerializeField] Vector2 minMaxSpeed = new Vector2(3f, 10f);
+    //float curSpeed = 5f;
+    [SerializeField] float maxSpeed = 5f;
+    [SerializeField] float smoothFactor = 0.5f;
+
+    Vector3 velocity;
+
+    Vector3 lastPos;
+
+    //v = d/t
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = GameObject.FindGameObjectWithTag("Player").transform.GetChild(5);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //lastPos = transform.position;
 
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothFactor, maxSpeed);
+        DoRotation();
     }
 
 
     void DoRotation()
     {
-        //Vector3 vectorToTarget = player.position - transform.position;
-        //float angle = Mathf.Atan2(vectorToTarget.z, vectorToTarget.x) * Mathf.Rad2Deg;
-        //Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * _maxRotateSpeed);
+        float angle = Vector3.SignedAngle(transform.forward, target.forward, Vector3.up);
+        angle = Mathf.Clamp(angle, -maxAngularSpeed, maxAngularSpeed);
+        transform.Rotate(Vector3.up * angle * Time.deltaTime * (1f / timeToReachTargetAngle));
+
+        //Vector3 direction = target.position - transform.position;
+        //direction.y = 0f;
+        //float angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
+        //angle = Mathf.Clamp(angle, -maxAngleSpeed, maxAngleSpeed);
+        //transform.Rotate(Vector3.up * angle * Time.deltaTime);
 
     }
 }
