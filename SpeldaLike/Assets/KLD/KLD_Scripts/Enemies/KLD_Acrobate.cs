d5143bd0b7285b7f5794f664a8e4ea5d64c115ac
Clément Pagnier <clempagnier@yahoo.fr>
2021-06-01 18:14:55 +0200

    Mastodonte 80%

--- SpeldaLike/Assets/KLD/KLD_Scripts/Enemies/KLD_Acrobate.cs
+++ SpeldaLike/Assets/KLD/KLD_Scripts/Enemies/KLD_Acrobate.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class KLD_Acrobate : KLD_Enemy
 {
-
+    [SerializeField] GameObject explodedPrefab;
+    [SerializeField] Animator animator;
     [Header("Speeds"), SerializeField] Vector2 spAcWandering = new Vector2(4f, 6f);
     [SerializeField] Vector2 spAcAlerted = new Vector2(6f, 10f);
     [SerializeField] Vector2 spAcBlocking = new Vector2(6f, 10f);
 
+    [SerializeField] float blockingDistanceToPlayer = 2f;
     [SerializeField] float blockingDistance = 4f;
-    [SerializeField] float parryTime = 0.3f;
-    float curParryTime;
     [SerializeField] float blockTimeToAttack = 2f;
     float curBlockTimeWithoutAttack;
 
     [SerializeField] float attackDuration = 0.5f;
+    float curAttackTime;
 
     [SerializeField] float stunDuration = 3f;
     float curStunTime;
 
-    enum AcrobateState { WANDERING, ALERTED, BLOCKING, PARRYING, ATTACKING, STUNNED };
-    AcrobateState curState = AcrobateState.WANDERING;
+    [SerializeField] GameObject shieldObj;
+
+    [SerializeField, Header("Rotation")]
+    float maxAngularSpeed = 180f;
+    [SerializeField] float timeToReachTargetAngle = 0.4f;
+
+    enum AcrobateState { WANDERING, ALERTED, BLOCKING, ATTACKING, STUNNED };
+    [SerializeField] AcrobateState curState = AcrobateState.WANDERING;
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+
+        isInvulnerable = true;
+        SetAgentSpac(spAcWandering);
+        //shieldObj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -58,6 +69,7 @@ public class KLD_Acrobate : KLD_Enemy
             else if (PlayerDistance() < blockingDistance)
             {
                 curState = AcrobateState.BLOCKING;
+                SetAgentSpac(spAcBlocking);
             }
         }
         else if (curState == AcrobateState.BLOCKING)
@@ -65,35 +77,39 @@ public class KLD_Acrobate : KLD_Enemy
             if (!IsPlayerInZone())
             {
                 curState = AcrobateState.WANDERING;
+                SetAgentSpac(spAcWandering);
             }
             else if (PlayerDistance() > blockingDistance)
             {
                 curState = AcrobateState.ALERTED;
+                SetAgentSpac(spAcAlerted);
             }
             else if (curBlockTimeWithoutAttack >= blockTimeToAttack)
             {
-                //attack
+                animator.SetTrigger("attack");
                 curState = AcrobateState.ATTACKING;
-            }
-            //if take damage => parry / reset curBlockTime / reset curParryTime
-        }
-        else if (curState == AcrobateState.PARRYING)
-        {
-            if (curParryTime >= parryTime)
-            {
-                curState = AcrobateState.BLOCKING;
+                //isInvulnerable = false;
+                //shieldObj.SetActive(false);
             }
         }
         else if (curState == AcrobateState.ATTACKING)
         {
-
+            if (curAttackTime >= attackDuration)
+            {
+                curState = AcrobateState.BLOCKING;
+                //isInvulnerable = true;
+                //shieldObj.SetActive(true);
+                curBlockTimeWithoutAttack = 0f;
+            }
         }
         else if (curState == AcrobateState.STUNNED)
         {
             if (curStunTime >= stunDuration)
             {
                 curState = AcrobateState.WANDERING;
-
+                r.materials[1].SetFloat("Stun_Slider_", 0f);
+                isInvulnerable = true;
+                shieldObj.SetActive(true);
             }
         }
     }
@@ -107,18 +123,21 @@ public class KLD_Acrobate : KLD_Enemy
                 break;
 
             case AcrobateState.ALERTED:
+                StickToPlayer();
                 break;
 
             case AcrobateState.BLOCKING:
-                break;
-
-            case AcrobateState.PARRYING:
+                curBlockTimeWithoutAttack += Time.deltaTime;
+                StickToPlayer();
                 break;
 
             case AcrobateState.ATTACKING:
+                curAttackTime += Time.deltaTime;
+                DoRotation();
                 break;
 
             case AcrobateState.STUNNED:
+                curStunTime += Time.deltaTime;
                 break;
 
             default:
@@ -129,22 +148,52 @@ public class KLD_Acrobate : KLD_Enemy
 
     protected override void OnDamageTake()
     {
-        throw new System.NotImplementedException();
+
     }
 
     protected override void OnSuperJumpShockwave()
     {
-        throw new System.NotImplementedException();
+
     }
 
-    void SetAgentSpac(Vector2 _spac)
+    public override void DamageEnemySuperJump(int _damage)
     {
-        agent.speed = _spac.x;
-        agent.acceleration = _spac.y;
+        //base.DamageEnemySuperJump(_damage);
+        curState = AcrobateState.STUNNED;
+        curStunTime = 0f;
+        isInvulnerable = false;
+        shieldObj.SetActive(false);
+        r.materials[1].SetFloat("Stun_Slider_", 1f);
     }
 
     void StickToPlayer()
     {
+        if (PlayerDistance() > blockingDistanceToPlayer)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            agent.isStopped = true;
+            DoRotation();
+        }
+    }
 
+    void DoRotation()
+    {
+        Vector3 toPlayer = player.position - transform.position;
+        toPlayer.y = 0f;
+
+        float angle = Vector3.SignedAngle(transform.forward, toPlayer, Vector3.up);
+
+        angle = Mathf.Clamp(angle, -maxAngularSpeed, maxAngularSpeed);
+        transform.Rotate(Vector3.up * angle * Time.deltaTime * (1f / timeToReachTargetAngle));
+    }
+
+    protected override void Die()
+    {
+        Instantiate(explodedPrefab, transform.position, transform.rotation);
+        base.Die();
     }
 }
